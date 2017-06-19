a61c07219ee14c837f78a6c42c4ee8ac676e067a
BitfuryLightning <lightning@bitfury.com>
2017-06-19 06:35:51 +0300

    Add missing ports

--- bitcoin/start.sh
+++ bitcoin/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-docker run -p 10009:10009 -p 11009:11009 -p 12009:12009 -it simple-simnet-bitcoin
+docker run -p 10009:10009 -p 11009:11009 -p 12009:12009 -p 13009:13009 -it simple-simnet-bitcoin
