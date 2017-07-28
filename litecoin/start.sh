f44022a65d440ef54eb6d11b49788186e5a0a400
Evan Schwartz <evan.mark.schwartz@gmail.com>
2017-07-28 10:03:17 -0700

    Use different ports for bitcoin and litecoin
    So that you can easily run both at the same time

--- litecoin/start.sh
+++ litecoin/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-docker run -p 10009:10009 -p 11009:11009 -p 12009:12009 -p 13009:13009 -it simple-simnet-litecoin
+docker run -p 10010:10009 -p 11010:11009 -p 12010:12009 -p 13010:13009 -it simple-simnet-litecoin
